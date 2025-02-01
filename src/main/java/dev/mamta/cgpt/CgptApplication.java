0ed643b1d334d96360371f7facf9bd143556a299
mamta09 <mamtapal9759@gmail.com>
2025-02-01 13:47:53 +0530

    initial commit

---
+++ src/main/java/dev/mamta/cgpt/CgptApplication.java
@@ -0,0 +1,14 @@
+package dev.mamta.cgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgptApplication.class, args);
+	}
+
+
+}
